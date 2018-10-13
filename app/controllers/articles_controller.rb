bfd9389d9de41d943de293a4a5dc2da35538f608
Isaias Pomales <impomales@gmail.com>
2018-10-13 18:08:54 -0400

    can update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,16 +14,31 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:notice] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
       render 'new'
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end

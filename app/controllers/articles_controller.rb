2a57ba36ffe22320347ff6e97277a197267ccc27
Isaias Pomales <impomales@gmail.com>
2018-10-13 16:44:32 -0400

    show article by id

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
 
   def new
     # creates a new article, not yet saved.
@@ -8,10 +11,14 @@ class ArticlesController < ApplicationController
   def create
     # see params on page.
     # render plain: params[:article].inspect
-
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
 
   private

a08d8cd2ec21ed77d7d09e24947ecfad97668919
Isaias Pomales <impomales@gmail.com>
2018-11-03 18:27:42 -0400

    add bootstrap navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require jquery
-//= bootstrap-sprockets
+//= require bootstrap-sprockets
+//= require activestorage
 //= require turbolinks
 //= require_tree .

eef83b22ec2e4c94b81ec18d5b28d84a03efc6e6
Isaias Pomales <impomales@gmail.com>
2018-11-12 16:57:03 -0500

    add validations to username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
+VALID_EMAIL_REGEX = /([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/
+
 class User < ApplicationRecord
+  # add validations here.
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       length: { minimum: 3, maximum: 25 }
+
+  validates :email, presence: true, length: { maximum: 105 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end

81bf1e70e26741258e378e4fc444399196f12ae5
Isaias Pomales <impomales@gmail.com>
2018-11-28 19:15:33 -0500

    user can sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+
+        if @user.save
+            flash[:success] = "Welcome to the Blog #{@user.username}!"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end

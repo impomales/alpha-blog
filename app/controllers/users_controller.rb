0ad1f1e4a27a393ffe4205b47c05261ffe44373f
Isaias Pomales <impomales@gmail.com>
2018-11-28 19:58:36 -0500

    a user can edit account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show
+    end
+
     def create
         @user = User.new(user_params)
 
@@ -11,6 +14,21 @@ class UsersController < ApplicationController
             redirect_to articles_path
         else
             render 'new'
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            flash[:success] = "Your account was updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
         end
     end
 

90559cdb930420b64d720d75db6b919f46b6a853
Isaias Pomales <impomales@gmail.com>
2018-12-01 00:22:21 -0500

    add user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,28 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to the Blog #{@user.username}!"
+      redirect_to articles_path
+    else
+      render 'new'
     end
+  end
 
-    def create
-        @user = User.new(user_params)
+  private
 
-        if @user.save
-            flash[:success] = "Welcome to the Blog #{@user.username}!"
-            redirect_to articles_path
-        else
-            render 'new'
-        end
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
